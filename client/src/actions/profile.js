969a0c56f46814330ede00cb65a17836902a7955
Alfonso Feria <alfonso.feria@konfio.mx>
2019-11-18 15:32:20 -0600

    Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,13 +7,48 @@ import { GET_PROFILE, PROFILE_ERROR } from "./types";
 export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get("/api/profile/me");
-    console.log(res);
     dispatch({
       type: GET_PROFILE,
       payload: res.data
     });
   } catch (err) {
-    console.log(err);
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Create or Update Profile
+
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile created", "success"));
+
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
