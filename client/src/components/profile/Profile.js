a4fd26c9dc240091092081f631e94c21a48be23e
Alfonso Feria <alfonso.feria@konfio.mx>
2019-11-24 13:52:00 -0600

    Displaying Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGitHub from "./ProfileGitHub";
 
 const Profile = ({
   getProfileById,
@@ -67,6 +68,9 @@ const Profile = ({
                 <h4>No Education credentials</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGitHub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
