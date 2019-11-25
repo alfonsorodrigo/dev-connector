80d44b699ed611f42a654a334c734bcf1e7aa5eb
Alfonso Feria <alfonso.feria@konfio.mx>
2019-11-24 20:02:55 -0600

    Single Post Display

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,12 +4,12 @@ import Spinner from "../layout/Spinner";
 import PostItem from "./PostItem";
 import PostForm from "./PostForm";
 import { connect } from "react-redux";
-import { getPost } from "../../actions/post";
+import { getPosts } from "../../actions/post";
 
-const Posts = ({ getPost, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
-    getPost();
-  }, [getPost]);
+    getPosts();
+  }, [getPosts]);
   return loading ? (
     <Spinner />
   ) : (
@@ -29,11 +29,11 @@ const Posts = ({ getPost, post: { posts, loading } }) => {
 };
 
 Posts.propTypes = {
-  getPost: PropTypes.func.isRequired,
+  getPosts: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
   post: state.post
 });
-export default connect(mapStateToProps, { getPost })(Posts);
+export default connect(mapStateToProps, { getPosts })(Posts);
